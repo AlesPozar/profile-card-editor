d703716e68ed6482edda7925d122c8db3c1d5076
AlesPozar <ales.pozar003@gmail.com>
2025-05-05 17:23:42 +0200

    neki

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@
 const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/profile-card-editor' : '',
   output: "export",  // <=== enables static exports
-  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
