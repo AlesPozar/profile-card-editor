ac50eae5723f4a8a9388635b2243c8ad6bcf3d7b
AlesPozar <ales.pozar003@gmail.com>
2025-05-05 17:11:44 +0200

    neki

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // GitHub Pages uses a subdirectory, so we need to specify the base path
-  // Replace 'profile-card-editor' with your repository name
   basePath: process.env.NODE_ENV === 'production' ? '/profile-card-editor' : '',
+  images: {
+    unoptimized: true,
+  },
 };
-
-module.exports = nextConfig
 
 export default nextConfig;
