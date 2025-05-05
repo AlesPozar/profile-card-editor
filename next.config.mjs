8873a1a70704d832e1bf7e2efdb0f23093664b97
AlesPozar <ales.pozar003@gmail.com>
2025-05-05 17:01:28 +0200

    neki2

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const nextConfig = {
   // GitHub Pages uses a subdirectory, so we need to specify the base path
   // Replace 'profile-card-editor' with your repository name
   basePath: process.env.NODE_ENV === 'production' ? '/profile-card-editor' : '',
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
